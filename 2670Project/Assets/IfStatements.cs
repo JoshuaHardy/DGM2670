7889d9d0c569bc0ff83c181f27ed8aab6cb39b2f
Joshua Hardy <joshua.james.hardy@gmail.com>
2020-08-31 00:30:11 -0600

    Folder Migration
    Finally done

---
+++ 2670Project/Assets/IfStatements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public bool canRun;
+    public string password;
+    public int number;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World.");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canRun)
+        {
+            Debug.Log("Running");
+        }
+
+        if (password == "OU812")
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("incorrect");
+        }
+    }
+}
---
+++ 2670Project/Assets/IntData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntData : MonoBehaviour
+{
+    public string password = "New Password";
+    private void Start()
+    {
+        Debug.Log():"Start"); 
+    }
+
+    public void MoveCharacterForward()
+    {
+       if (passwordEntry == :"OU812) 
+    }
+}
