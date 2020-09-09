3d822412f9e82ff28c3a451e3677c4aff08f00b5
Joshua Hardy <joshua.james.hardy@gmail.com>
2020-09-08 23:57:28 -0600

    More C# Tutorials

---
+++ Notes/BookScripts/PrintTest01/PrintTest01/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PrintTest01
+{
+    public class Class1
+    {
+    }
+}
---
+++ Notes/BookScripts/PrintTest02/PrintTest02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintTest02
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            /*int x = 12 * 30;
+            Console.WriteLine(x);*/
+            Console.WriteLine (FeetToInches(30)); //360
+            Console.WriteLine(FeetToInches(100)); //1200
+        }
+    }
+}
